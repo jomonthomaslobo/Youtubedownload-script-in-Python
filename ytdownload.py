2435b6995fb0e6e51f9d1ba55631ede5c0b3b89c
Jomon Lobo <jomonthomaslobo@gmail.com>
2024-06-03 09:02:32 +0530

    Initial Commit

---
+++ ytdownload.py
@@ -0,0 +1,14 @@
+#Python Script to download Youtube Videos for educational purpose only
+
+import yt_dlp
+
+#read video URL
+
+url = input("Enter Video URL : ")
+ydl_opts={}
+
+#download the video
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+
+print("Download Successfull")
